435d922b898f4cb651041fd51707752397f7c81e
Harry Garrood <harry@garrood.me>
2014-02-11 08:06:37 +0000

    IT WORKS YAY

--- js/ui.js
+++ js/ui.js
@@ -20,9 +20,12 @@ window.Unidex.query = function query(words) {
 
 var resultsElem = document.getElementById('results')
 function displayResults(results) {
-    removeAllChildNodes(resultsElem);
+    removeAllChildNodes(resultsElem)
     var rows = _.map(results, toResultRow)
-    rows.forEach(function(row) { resultsElem.innerHtml += row })
+    rows.forEach(function(row) {
+        if (row != null)
+            resultsElem.appendChild(row)
+    })
 }
 
 function removeAllChildNodes(elem) {
@@ -31,10 +34,31 @@ function removeAllChildNodes(elem) {
 }
 
 function toResultRow(codepoint) {
+    if (codepoint == null)
+        return null;
 
+   var row = document.createElement('tr')
+
+   var name_cell = document.createElement('td')
+   var name_cell_text = document.createTextNode(codepoint.name)
+   name_cell.appendChild(name_cell_text)
+   
+   var char_cell = document.createElement('td')
+   var char_cell_text = document.createTextNode("&#" + codepoint.hex + ";")
+   char_cell.appendChild(char_cell_text)
+
+   var hexcode_cell = document.createElement('td')
+   var hexcode_cell_text = document.createTextNode(codepoint.hex)
+   hexcode_cell.appendChild(hexcode_cell_text)
+
+   row.appendChild(name_cell)
+   row.appendChild(char_cell)
+   row.appendChild(hexcode_cell)
+   return row;
 }
 
 var inputElem = document.getElementById('input')
 inputElem.addEventListener('keyup', function(e) {
-
+    var results = Unidex.query(inputElem.value.split(" "))
+    displayResults(results)
 })
